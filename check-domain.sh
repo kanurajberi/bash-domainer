9650152f08dfb65f6bdeb66c1aba9b8ddc1e6615
Kanuraj Beri <kanurajberi@gmail.com>
2017-07-05 18:25:43 +0530

    Added 'scheme' comment

--- check-domain.sh
+++ check-domain.sh
@@ -14,7 +14,7 @@ get_answers_number () {
 file="unregistered.txt"
 echo "Results will be saved in $file"
 
-for adr in {b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z}a{b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z}a
+for adr in {b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z}a{b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z}a #scheme
 do
     name="$adr.com"
     printf "Checking %s ...\r" "$name"
